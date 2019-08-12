d9d4185dd4958312e223f056c8986bcd7586263f
Niklas Reimer <niklas@nr205.de>
2019-08-12 20:04:58 +0000

    Import more stuff

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ process.on('unhandledRejection', function (reason, p) {
 window.emitter = new events.EventEmitter();
 
 const config = signalRequire('config/production');
-window.getTitle = () => title;
+window.getTitle = () => "";
 window.getEnvironment = () => config.environment;
 window.getAppInstance = () => config.appInstance;
 window.getVersion = () => config.version;
@@ -32,6 +32,9 @@ window.getNodeVersion = () => config.node_version;
 window.getHostName = () => config.hostname;
 window.getServerTrustRoot = () => config.serverTrustRoot;
 window.isBehindProxy = () => Boolean(config.proxyUrl);
+window.setBadgeCount = count => "";
+window.updateTrayIcon = window.updateTrayIcon = unreadCount => "";
+
 const auth = "-----BEGIN CERTIFICATE-----\nMIID7zCCAtegAwIBAgIJAIm6LatK5PNiMA0GCSqGSIb3DQEBBQUAMIGNMQswCQYD\nVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5j\naXNjbzEdMBsGA1UECgwUT3BlbiBXaGlzcGVyIFN5c3RlbXMxHTAbBgNVBAsMFE9w\nZW4gV2hpc3BlciBTeXN0ZW1zMRMwEQYDVQQDDApUZXh0U2VjdXJlMB4XDTEzMDMy\nNTIyMTgzNVoXDTIzMDMyMzIyMTgzNVowgY0xCzAJBgNVBAYTAlVTMRMwEQYDVQQI\nDApDYWxpZm9ybmlhMRYwFAYDVQQHDA1TYW4gRnJhbmNpc2NvMR0wGwYDVQQKDBRP\ncGVuIFdoaXNwZXIgU3lzdGVtczEdMBsGA1UECwwUT3BlbiBXaGlzcGVyIFN5c3Rl\nbXMxEzARBgNVBAMMClRleHRTZWN1cmUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAw\nggEKAoIBAQDBSWBpOCBDF0i4q2d4jAXkSXUGpbeWugVPQCjaL6qD9QDOxeW1afvf\nPo863i6Crq1KDxHpB36EwzVcjwLkFTIMeo7t9s1FQolAt3mErV2U0vie6Ves+yj6\ngrSfxwIDAcdsKmI0a1SQCZlr3Q1tcHAkAKFRxYNawADyps5B+Zmqcgf653TXS5/0\nIPPQLocLn8GWLwOYNnYfBvILKDMItmZTtEbucdigxEA9mfIvvHADEbteLtVgwBm9\nR5vVvtwrD6CCxI3pgH7EH7kMP0Od93wLisvn1yhHY7FuYlrkYqdkMvWUrKoASVw4\njb69vaeJCUdU+HCoXOSP1PQcL6WenNCHAgMBAAGjUDBOMB0GA1UdDgQWBBQBixjx\nP/s5GURuhYa+lGUypzI8kDAfBgNVHSMEGDAWgBQBixjxP/s5GURuhYa+lGUypzI8\nkDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4IBAQB+Hr4hC56m0LvJAu1R\nK6NuPDbTMEN7/jMojFHxH4P3XPFfupjR+bkDq0pPOU6JjIxnrD1XD/EVmTTaTVY5\niOheyv7UzJOefb2pLOc9qsuvI4fnaESh9bhzln+LXxtCrRPGhkxA1IMIo3J/s2WF\n/KVYZyciu6b4ubJ91XPAuBNZwImug7/srWvbpk0hq6A6z140WTVSKtJG7EP41kJe\n/oF4usY5J7LPkxK3LWzMJnb5EIJDmRvyH8pyRwWg6Qm6qiGFaI4nL8QU4La1x2en\n4DGXRaLMPRwjELNgQPodR38zoCMuA8gHZfZYYoZ7D7Q1wNUiVHcxuFrEeBaYJbLE\nrwLV\n-----END CERTIFICATE-----\n";
 
 const Attachments = signalRequire('app/attachments');
@@ -78,6 +81,13 @@ window.Signal = Signal.setup({
   getRegionCode: () => window.storage.get('regionCode'),
   logger: window.log,
 });
+
+const { Errors, Message } = window.Signal.Types;
+const {
+  upgradeMessageSchema,
+  writeNewAttachmentData,
+  deleteAttachmentData,
+} = window.Signal.Migrations;
 
 window.i18n = function(locale, messages) {
   return '';
@@ -179,6 +189,10 @@ Whisper.events.trigger('storage_ready');
 signalRequire('js/signal_protocol_store');
 signalRequire('js/libtextsecure');
 
+signalRequire('js/delivery_receipts');
+signalRequire('js/read_receipts');
+signalRequire('js/read_syncs');
+signalRequire('js/view_syncs');
 window.libphonenumber = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 window.libphonenumber.PhoneNumberFormat = require('google-libphonenumber').PhoneNumberFormat;
 signalRequire('js/libphonenumber-util');
@@ -186,6 +200,7 @@ signalRequire('js/models/messages');
 signalRequire('js/models/conversations');
 signalRequire('js/models/blockedNumbers');
 signalRequire('js/expiring_messages');
+signalRequire('js/expiring_tap_to_view_messages');
 
 signalRequire('js/chromium');
 signalRequire('js/registration');
